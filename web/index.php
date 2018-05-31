8a0366072c479cbec64e0179725c2af6197b49fb
Yui Nishimura <nisshi.yui79@gmail.com>
2018-05-31 13:52:24 +0900

    ifttt5

--- web/index.php
+++ web/index.php
@@ -2,27 +2,6 @@
 
 require('../vendor/autoload.php');
 
-/*$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
-
-$app->run();*/
 
 const DAY = 1;
 const WEEK = 7;
@@ -81,10 +60,8 @@ $post_data = array(
     "value3" => "rr"
 );
 //IFTTT
-/*$url = "https://maker.ifttt.com/trigger/toubanbot1/with/key/rBrhvXD3WeFcdEEwJl6ht";*/
 $ch = curl_init('https://maker.ifttt.com/trigger/toubanbot1/with/key/rBrhvXD3WeFcdEEwJl6ht');
 
-/*curl_setopt($ch, CURLOPT_URL, $url);*/
 curl_setopt($ch,CURLOPT_POST, true);
 
 //データの配列を設定する
@@ -93,17 +70,3 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_data));
 curl_exec($ch);
 curl_close($ch);
 
-
-/*$from = new SendGrid\Email(null, 'nisshi.yui79@gmail.com');
-$subject = "当番のお知らせ";
-$to = new SendGrid\Email(null, "nisshi.yui79@gmail.com");
-$content = new SendGrid\Content("text/plain", "$toubanNotfication");
-$mail = new SendGrid\Mail($from, $subject, $to, $content);
-
-$apiKey = getenv('SENDGRID_API_KEY');
-$sg = new \SendGrid($apiKey);
-
-$response = $sg->client->mail()->send()->post($mail);
-echo $response->statusCode();
-echo $response->headers();
-echo $response->body();*/
