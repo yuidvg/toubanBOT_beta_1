039ab5dd7432a8589852c3fe3621453dfdec9a43
Yui Nishimura <nisshi.yui79@gmail.com>
2018-06-05 14:07:09 +0900

    ifttt

--- web/index.php
+++ web/index.php
@@ -12,14 +12,19 @@ function getMJD() {
         global $dt;
         $D = $dt->day;//日
         $M = $dt->month;//月
-        var_dump($Y = $dt->year);//年
+        $Y = $dt->year;//年
+        var_dump($Y);
+        var_dump($M);
+        var_dump($D);
 
         if ($M == 1 || $M == 2) {
             $Y = $Y - 1;
             echo $M = $M + 12;
         }
         $A = floor($Y / 100);
+        var_dump($A);
         $B = 2 - $A + floor($A / 4);
+        var_dump($B);
         $JD = floor(365.25 * $Y) + floor(30.6001 * ($M + 1)) + $D + $B - 1720994.5;
         $jD = floor($JD - 2400000.5);
 
